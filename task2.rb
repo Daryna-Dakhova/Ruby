9f6e8ad09d3d05ab469d16b36a036c08b69d4e03
Daryna-Dakhova <162442100+Daryna-Dakhova@users.noreply.github.com>
2024-09-25 21:10:50 +0300

    Add files via upload

---
+++ task2.rb
@@ -0,0 +1,48 @@
+# Функція для визначення пріоритету операторів (як новачок)
+def precedence(operator)
+  case operator
+  when '+', '-'
+    return 1
+  when '*', '/'
+    return 2
+  else
+    return 0
+  end
+end
+
+# Функція, щоб перевірити, чи це оператор
+def is_operator?(char)
+  return char == '+' || char == '-' || char == '*' || char == '/'
+end
+
+# Функція для перетворення інфіксного виразу у RPN
+def to_rpn(expression)
+  output = []
+  operators = []
+
+  tokens = expression.split(' ')  # Розбиваємо вираз на частини
+  
+  tokens.each do |token|
+    if token.match?(/\d+/)  # Якщо це число
+      output << token
+    elsif is_operator?(token)  # Якщо це оператор
+      while !operators.empty? && precedence(operators.last) >= precedence(token)
+        output << operators.pop
+      end
+      operators << token
+    end
+  end
+  
+  # Виводимо всі залишкові оператори
+  while !operators.empty?
+    output << operators.pop
+  end
+
+  return output.join(' ')  # Повертаємо результат
+end
+
+# Приклад використання
+input = "2 + 1 * 4"
+rpn_output = to_rpn(input)
+puts "Вхідний вираз: #{input}"
+puts "RPN результат: #{rpn_output}"
