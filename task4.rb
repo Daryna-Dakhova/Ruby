04f25aa88e4c49586c727dd2cfe8253b1324f479
Daryna-Dakhova <162442100+Daryna-Dakhova@users.noreply.github.com>
2024-10-09 22:08:30 +0300

    Add files via upload

---
+++ task4.rb
@@ -0,0 +1,22 @@
+def valid_ipv4?(ip)
+  parts = ip.split(".")
+  if parts.length != 4
+    return false
+  end
+
+  parts.each do |part|
+    if part.to_i.to_s != part || part.to_i < 0 || part.to_i > 255
+      return false
+    end
+  end
+
+  return true
+end
+
+# Приклади використання
+puts valid_ipv4?("10.0.0.1")       # true
+puts valid_ipv4?("172.16.254.1")   # true
+puts valid_ipv4?("192.168.100.256") # false (число більше 255)
+puts valid_ipv4?("001.1.1.1")      # false (ведучі нулі)
+puts valid_ipv4?("192.168")        # false (неповна адреса)
+puts valid_ipv4?("192.300.1.1")    # false (число більше 255)
